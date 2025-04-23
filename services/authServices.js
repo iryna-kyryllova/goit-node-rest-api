88d70b135258935ad57b74372d0982378e476255
Iryna Kyryllova <keira.kirillova@gmail.com>
2025-04-23 18:04:16 +0200

    add hashed password to DB

--- services/authServices.js
+++ services/authServices.js
@@ -1,14 +1,18 @@
+import bcrypt from 'bcrypt'
 import User from '../db/models/User.js'
 import HttpError from '../helpers/HttpError.js'
 
 export const register = async (data) => {
-  const { email } = data
+  const { email, password } = data
+
   const user = await User.findOne({ where: { email } })
   if (user) {
     throw HttpError(409, 'Email in use')
   }
 
-  return await User.create(data)
+  const hashedPassword = await bcrypt.hash(password, 10)
+
+  return User.create({ ...data, password: hashedPassword })
 }
 
 export default { register }
