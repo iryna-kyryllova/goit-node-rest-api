f695b9e920225aa22510f93e7626adb27add001b
Iryna Kyryllova <keira.kirillova@gmail.com>
2025-04-28 16:04:29 +0200

    add server port

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -67,15 +67,16 @@ export const getCurrentUser = controllerWrapper(async (req, res) => {
 })
 
 export const updateAvatar = controllerWrapper(async (req, res) => {
+  const { file } = req
+  if (!file) throw HttpError(400, 'Missing field avatar')
+
   const { id } = req.user
   let avatar = null
 
-  if (req.file) {
-    const { path: oldPath, filename } = req.file
-    const newPath = path.join(avatarsDir, filename)
-    await fs.rename(oldPath, newPath)
-    avatar = path.join('avatars', filename)
-  }
+  const { path: oldPath, filename } = file
+  const newPath = path.join(avatarsDir, filename)
+  await fs.rename(oldPath, newPath)
+  avatar = path.join('avatars', filename)
 
   const data = await authService.updateAvatar(id, avatar)
 
